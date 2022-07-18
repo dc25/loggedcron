2acf1eb78fe54e1539e0b60b51f1df1c09d88f7c
C N <nu404040@gmail.com>
2022-07-18 13:29:36 -0700

    logging command name now

--- loggedcron.bash
+++ loggedcron.bash
@@ -1,8 +1,9 @@
 #! /bin/bash
 COMMAND=$1
-NAME=`date +%Y-%m-%d__%H-%M-%S`
+FILE=`date +%Y-%m-%d__%H-%M-%S`
 DIR=~/cron_logs/${COMMAND##*/}
-FILE=$DIR/$NAME
 mkdir -p $DIR
+cd $DIR
 date > $FILE 2>&1
+echo RUNNING $COMMAND >> $FILE 2>&1
 $COMMAND >> $FILE 2>&1
